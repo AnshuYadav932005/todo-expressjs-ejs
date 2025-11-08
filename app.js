6c306a7b932bb5b2d615c2125e94158a921e81e8
anshuyadav <anshuy.932005@gmail.com>
2025-11-09 02:59:36 +0530

    brain not working

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ import express from 'express'
 const app=express();
 const port=5000;
 //using the ejs and view engine
+app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.set('views','./views')
 
-
-app.get('/',(req,res)=>{
-    const tasks=[
+const tasks=[
         {
             id: 1, 
             title: 'feed the dog',
@@ -25,12 +24,22 @@ app.get('/',(req,res)=>{
             completed: true
         }
     ]
+
+app.get('/',(req,res)=>{
+    
     res.render('tasks',{tasks});
 });
 
-app.post(/tasks/:id,(req,res)){
-    
-}
+app.post('/tasks/:id',(req,res)=>{
+    const taskId=parseInt(req.params.id);
+    const taskIndex=tasks.findIndex(task => task.id===taskId);
+    if(taskIndex===-1){
+        return res.status(404).send('Task not found');
+    }
+    console.log(req.body);
+    //res.redirect('/');
+
+});
 
 
 app.listen(port,()=>{
